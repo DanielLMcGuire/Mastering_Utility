5afd19edfde8995888ddec295465ea421f3d2de4
Daniel McGuire <danielmcguire23@icloud.com>
2025-10-17 16:55:29 -0500

    Updated hasher.ps1
    Updated hasher.ps1 to output SHA256 hashes in the format '<hash>  <filename>'

--- scripts/hasher.ps1
+++ scripts/hasher.ps1
@@ -1,2 +1,4 @@
-Get-FileHash -Path "masteringutil64.msi" -Algorithm SHA256 | Out-File -FilePath "masteringutil64.msi.sha256"
-Get-FileHash -Path "masteringutil64_setup.exe" -Algorithm SHA256 | Out-File -FilePath "masteringutil64-setup.exe.sha256"
+$hash = Get-FileHash -Path "masteringutil64.msi" -Algorithm SHA256
+"$($hash.Hash)  masteringutil64.msi" | Out-File -FilePath "masteringutil64.msi.sha256" -Encoding ASCII
+$hash = Get-FileHash -Path "masteringutil64_setup.exe" -Algorithm SHA256
+"$($hash.Hash)  masteringutil64_setup.exe" | Out-File -FilePath "masteringutil64-setup.exe.sha256" -Encoding ASCII
