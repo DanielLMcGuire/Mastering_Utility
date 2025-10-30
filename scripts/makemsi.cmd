4827b3492fdfbf759d1ea5b4674d5ec7aa97bef5
Daniel McGuire <146508360+DanielLMcGuire@users.noreply.github.com>
2025-10-30 07:53:34 -0500

    Update makemsi.cmd
    Signed-off-by: Daniel McGuire <146508360+DanielLMcGuire@users.noreply.github.com>

--- scripts/makemsi.cmd
+++ scripts/makemsi.cmd
@@ -1,5 +1,6 @@
 @echo off
 cd /d "%~dp0"
+dotnet tool install --global wix
 wix extension add WixToolset.UI.wixext
 mkdir "..\build\dist"
 wix build install.wxs -arch x64 -ext WixToolset.UI.wixext -o "..\build\dist\masteringutil64.msi"
