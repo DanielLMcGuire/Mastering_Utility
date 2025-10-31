fe364fa0bdf44244ddf2f4658f3e5476187008c6
Daniel McGuire <danielmcguire23@icloud.com>
2025-10-31 15:49:01 -0500

    misc bugfixes

--- scripts/makemsi.cmd
+++ scripts/makemsi.cmd
@@ -2,5 +2,5 @@
 cd /d "%~dp0"
 dotnet tool install --global wix
 wix extension add WixToolset.UI.wixext
-mkdir "..\build\dist"
-wix build install.wxs -arch x64 -ext WixToolset.UI.wixext -o "..\build\dist\masteringutil64.msi"
+mkdir "%1\dist"
+wix build install.wxs -arch x64 -ext WixToolset.UI.wixext -o "%1\dist\masteringutil64.msi"
