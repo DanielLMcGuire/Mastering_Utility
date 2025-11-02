db171286a8879d4ec271c474e6a2aa1d54a7748f
Daniel McGuire <danielmcguire23@icloud.com>
2025-11-02 12:53:57 -0600

    Cleaned up msi build process

--- scripts/makemsi.cmd
+++ scripts/makemsi.cmd
@@ -1,6 +1,14 @@
 @echo off
-cd /d "%~dp0"
-dotnet tool install --global wix
+cd /d "%~1"
+where /q wix >NUL
+if ERRORLEVEL 1 (
+	dotnet tool install --global wix
+)
+if not exist ".wix" (
 wix extension add WixToolset.UI.wixext
-mkdir "%1\dist"
-wix build install.wxs -arch x64 -ext WixToolset.UI.wixext -o "%1\dist\masteringutil64.msi"
+)
+if not exist "dist" (
+mkdir "dist"
+)
+wix build install.wxs -arch x64 -ext WixToolset.UI.wixext -o "dist\masteringutil64.msi"
+
