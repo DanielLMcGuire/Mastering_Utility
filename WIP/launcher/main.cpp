89d551db093a932183678707f8d8173c4cfb675e
Daniel McGuire <danielmcguire23@icloud.com>
2025-11-02 13:47:21 -0600

    Move WIP\launcher to WIP\rust\launcher

--- WIP/launcher/main.cpp
+++
@@ -1,8 +0,0 @@
-#include "MasteringUtility.h"
-#include "cxx.h"
-#include <memory>
-#include <string>
-
-std::unique_ptr<MasteringUtility> new_mastering_utility() {
-    return std::make_unique<MasteringUtility>();
-}
