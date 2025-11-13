f2a9bcdaaeea8d15c20d9531cd906cec9eb71901
Daniel McGuire <146508360+DanielLMcGuire@users.noreply.github.com>
2025-11-13 10:31:26 -0600

    Update tests.cpp
    Signed-off-by: Daniel McGuire <146508360+DanielLMcGuire@users.noreply.github.com>

--- src/frontend/tests.cpp
+++ src/frontend/tests.cpp
@@ -19,7 +19,7 @@
  // You should have received a copy of the GNU General Public License
  // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-#include "../MasteringUtil.h"
+#include "../backend/MasteringUtil.h"
 #include <dconsole.h>
 #include <chrono>
 #include <filesystem>
