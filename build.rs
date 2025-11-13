0f319c6e5634cce10fb7da6900cafea505ae7726
Daniel McGuire <danielmcguire23@icloud.com>
2025-11-12 18:43:33 -0600

    Update build.rs

--- build.rs
+++ build.rs
@@ -1,13 +1,8 @@
 fn main() {
     cxx_build::bridge("src/MasteringUtil.rs")
-        .file("cpp/MasteringUtil.cpp")
-        .file("cpp/MasteringUtil_cxx.cpp")
-        .flag_if_supported("-std=c++20")
+        .file("src/MasteringUtil.cpp")
+        .std("c++20")
+        .flag_if_supported("/EHsc")
+        .include("src")
         .compile("masteringutil");
-
-    println!("cargo:rerun-if-changed=cpp/MasteringUtil.cpp");
-    println!("cargo:rerun-if-changed=cpp/MasteringUtil_cxx.cpp");
-    println!("cargo:rerun-if-changed=cpp/MasteringUtil_cxx.h");
-    println!("cargo:rerun-if-changed=cpp/MasteringUtil.h");
-    println!("cargo:rerun-if-changed=src/MasteringUtil.rs");
 }
