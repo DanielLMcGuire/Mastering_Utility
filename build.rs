362d2f2479c165d18d7354aff38570dd3d014d72
Daniel McGuire <danielmcguire23@icloud.com>
2025-11-18 17:40:29 -0600

    Update CXX flags

--- build.rs
+++ build.rs
@@ -1,9 +1,11 @@
 fn main() {
     cxx_build::bridge("src/backend/rs/MasteringUtil.rs")
         .file("src/backend/cpp/MasteringUtil.cpp")
-        .std("c++20")
-        .flag_if_supported("/EHsc")
         .include("src/backend/cpp")
         .include("src/backend/rs")
+        .std("c++20")
+    	.flag_if_supported("/EHsc")
+		.flag_if_supported("-fexceptions")
+		.flag_if_supported("-fcxx-exceptions")
         .compile("masteringutil");
 }
